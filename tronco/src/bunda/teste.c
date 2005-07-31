cccf69d0a51eb11275643a3fd146ede972812e5b
acdcp <acdcp@137627cd-51f6-0310-b489-f4c3dfe84c61>
2005-07-31 23:25:39 +0000

    mais testes; remoção dos arquivos que foram renomeados

--- tronco/src/bunda/teste.c
+++ tronco/src/bunda/teste.c
@@ -1,8 +1,8 @@
 /*
 
-Compilar assim:
+   Compilar assim:
 
-gcc -S -march=i386 -fverbose-asm -O0 teste.c 
+   gcc -S -march=i386 -fverbose-asm -O0 teste.c 
 
 */
 
@@ -14,33 +14,39 @@ int currentYear;
 
 void display (int n)
 {
-     printf ("[%d  %s  %d %d]", 8, "James Brown", currentYear, fatorial2 (8));
+	printf ("[%d  %s  %d %d]", 8, "James Brown", currentYear, fatorial2 (8));
 }
 
 int fatorial (int param)
 {
 	if (param == 0)
-	   return 1;
+		return 1;
 	else
-	    return param * fatorial(param-1);
+		return param * fatorial(param-1);
 }
 
 int fatorial2 (int param1)
 {
-    int result = 1;
+	int result = 1;
 
-    while (param1 > 1)
-    {
-        result = result * param1;    
+	while (param1 > 1)
+	{
+		result = result * param1;    
 		param1 = param1 - 1;
-    }
-  
-    return result;
+	}
+
+	return result;
 }
 
 
 int main ()
 {
-    currentYear = 2005;
-    display (555);
+	int ano;
+	int num;
+
+	ano = 2005;
+	num = 555;
+
+	currentYear = ano;
+	display (num);
 }
--- tronco/src/bunda/teste.s
+++ tronco/src/bunda/teste.s
@@ -1,108 +1,108 @@
 	.file	"teste.c"
- # GNU C version 3.4.2 (mingw-special) (mingw32)
- #	compiled by GNU C version 3.4.2 (mingw-special).
- # GGC heuristics: --param ggc-min-expand=47 --param ggc-min-heapsize=32702
- # options passed:  -iprefix -march=i386 -auxbase -O0 -fverbose-asm
- # options enabled:  -feliminate-unused-debug-types -fpeephole
- # -ffunction-cse -fkeep-static-consts -freg-struct-return -fgcse-lm
- # -fgcse-sm -fgcse-las -fsched-interblock -fsched-spec
- # -fsched-stalled-insns -fsched-stalled-insns-dep -fbranch-count-reg
- # -fcommon -fverbose-asm -fargument-alias -fzero-initialized-in-bss
- # -fident -fmath-errno -ftrapping-math -m80387 -mhard-float
- # -mno-soft-float -malign-double -mieee-fp -mfp-ret-in-387
- # -mstack-arg-probe -mno-red-zone -mtune=i386 -march=i386
+# GNU C version 3.3.5 (Debian 1:3.3.5-8ubuntu2) (i486-linux)
+#	compiled by GNU C version 3.3.5 (Debian 1:3.3.5-8ubuntu2).
+# GGC heuristics: --param ggc-min-expand=47 --param ggc-min-heapsize=32101
+# options passed:  -D__GNUC__=3 -D__GNUC_MINOR__=3 -D__GNUC_PATCHLEVEL__=5
+# -march=i386 -auxbase -O0 -fverbose-asm
+# options enabled:  -fpeephole -ffunction-cse -fkeep-static-consts
+# -fpcc-struct-return -fgcse-lm -fgcse-sm -fsched-interblock -fsched-spec
+# -fbranch-count-reg -fcommon -fverbose-asm -fgnu-linker -fargument-alias
+# -fzero-initialized-in-bss -fident -fmath-errno -ftrapping-math -m80387
+# -mhard-float -mno-soft-float -mieee-fp -mfp-ret-in-387 -mcpu=i386
+# -march=i386
 
-	.section .rdata,"dr"
-LC0:
-	.ascii "James Brown\0"
-LC1:
-	.ascii "[%d  %s  %d %d]\0"
+	.section	.rodata
+.LC0:
+	.string	"James Brown"
+.LC1:
+	.string	"[%d  %s  %d %d]"
 	.text
-.globl _display
-	.def	_display;	.scl	2;	.type	32;	.endef
-_display:
-	pushl	%ebp	 #
-	movl	%esp, %ebp	 #,
-	subl	$8, %esp	 #,
-	subl	$12, %esp	 #,
-	pushl	$8	 #
-	call	_fatorial2	 #
-	addl	$4, %esp	 #,
-	pushl	%eax	 # tmp58
-	pushl	_currentYear	 # currentYear
-	pushl	$LC0	 #
-	pushl	$8	 #
-	pushl	$LC1	 #
-	call	_printf	 #
-	addl	$32, %esp	 #,
+.globl display
+	.type	display, @function
+display:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$8, %esp
+	subl	$12, %esp
+	pushl	$8
+	call	fatorial2
+	addl	$4, %esp
+	pushl	%eax
+	pushl	currentYear	#  currentYear
+	pushl	$.LC0
+	pushl	$8
+	pushl	$.LC1
+	call	printf
+	addl	$32, %esp
 	leave
 	ret
-.globl _fatorial
-	.def	_fatorial;	.scl	2;	.type	32;	.endef
-_fatorial:
-	pushl	%ebp	 #
-	movl	%esp, %ebp	 #,
-	subl	$4, %esp	 #,
-	cmpl	$0, 8(%ebp)	 #, param
-	jne	L3	 #,
-	movl	$1, -4(%ebp)	 #, <result>
-	jmp	L2	 #
-L3:
-	subl	$12, %esp	 #,
-	movl	8(%ebp), %eax	 # param, tmp61
-	decl	%eax	 # tmp61
-	pushl	%eax	 # tmp61
-	call	_fatorial	 #
-	addl	$16, %esp	 #,
-	imull	8(%ebp), %eax	 # param, tmp60
-	movl	%eax, -4(%ebp)	 # tmp60, <result>
-L2:
-	movl	-4(%ebp), %eax	 # <result>, <result>
+	.size	display, .-display
+.globl fatorial
+	.type	fatorial, @function
+fatorial:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$4, %esp
+	cmpl	$0, 8(%ebp)	#  param
+	jne	.L3
+	movl	$1, -4(%ebp)
+	jmp	.L2
+.L3:
+	subl	$12, %esp
+	movl	8(%ebp), %eax	#  param
+	decl	%eax
+	pushl	%eax
+	call	fatorial
+	addl	$16, %esp
+	imull	8(%ebp), %eax	#  param
+	movl	%eax, -4(%ebp)
+.L2:
+	movl	-4(%ebp), %eax
 	leave
 	ret
-.globl _fatorial2
-	.def	_fatorial2;	.scl	2;	.type	32;	.endef
-_fatorial2:
-	pushl	%ebp	 #
-	movl	%esp, %ebp	 #,
-	subl	$4, %esp	 #,
-	movl	$1, -4(%ebp)	 #, result
-L6:
-	cmpl	$1, 8(%ebp)	 #, param1
-	jle	L7	 #,
-	movl	-4(%ebp), %eax	 # result, result
-	imull	8(%ebp), %eax	 # param1, tmp60
-	movl	%eax, -4(%ebp)	 # tmp60, result
-	decl	8(%ebp)	 # param1
-	jmp	L6	 #
-L7:
-	movl	-4(%ebp), %eax	 # result, result
+	.size	fatorial, .-fatorial
+.globl fatorial2
+	.type	fatorial2, @function
+fatorial2:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$4, %esp
+	movl	$1, -4(%ebp)	#  result
+.L6:
+	cmpl	$1, 8(%ebp)	#  param1
+	jg	.L8
+	jmp	.L7
+.L8:
+	movl	-4(%ebp), %eax	#  result
+	imull	8(%ebp), %eax	#  param1
+	movl	%eax, -4(%ebp)	#  result
+	decl	8(%ebp)	#  param1
+	jmp	.L6
+.L7:
+	movl	-4(%ebp), %eax	#  result
 	leave
 	ret
-	.def	___main;	.scl	2;	.type	32;	.endef
-.globl _main
-	.def	_main;	.scl	2;	.type	32;	.endef
-_main:
-	pushl	%ebp	 #
-	movl	%esp, %ebp	 #,
-	subl	$8, %esp	 #,
-	andl	$-16, %esp	 #,
-	movl	$0, %eax	 #, tmp59
-	addl	$15, %eax	 #, tmp60
-	addl	$15, %eax	 #, tmp61
-	shrl	$4, %eax	 #, tmp62
-	sall	$4, %eax	 #, tmp64
-	movl	%eax, -4(%ebp)	 # tmp64,
-	movl	-4(%ebp), %eax	 #,
-	call	__alloca
-	call	___main	 #
-	movl	$2005, _currentYear	 #, currentYear
-	subl	$12, %esp	 #,
-	pushl	$555	 #
-	call	_display	 #
-	addl	$16, %esp	 #,
+	.size	fatorial2, .-fatorial2
+.globl main
+	.type	main, @function
+main:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$8, %esp
+	andl	$-16, %esp
+	movl	$0, %eax
+	subl	%eax, %esp
+	movl	$2005, -4(%ebp)	#  ano
+	movl	$555, -8(%ebp)	#  num
+	movl	-4(%ebp), %eax	#  ano
+	movl	%eax, currentYear	#  currentYear
+	subl	$12, %esp
+	pushl	-8(%ebp)	#  num
+	call	display
+	addl	$16, %esp
 	leave
 	ret
-	.comm	_currentYear, 16	 # 4
-	.def	_fatorial2;	.scl	3;	.type	32;	.endef
-	.def	_printf;	.scl	3;	.type	32;	.endef
+	.size	main, .-main
+	.comm	currentYear,4,4
+	.section	.note.GNU-stack,"",@progbits
+	.ident	"GCC: (GNU) 3.3.5 (Debian 1:3.3.5-8ubuntu2)"
